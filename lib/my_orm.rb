2f4ca9c87288d50e162ff75af63169ef07de1891
ilya <viaznikov@sfedu.ru>
2024-03-16 18:55:20 +0300

    По существующей таблице воссоздает класс

--- lib/my_orm.rb
+++ lib/my_orm.rb
@@ -9,17 +9,14 @@ require_relative "my_orm/class_methods.rb"
 
 
 
-module My_Orm
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
 My_Orm::DataBase.connection("test_db.db")
 
-class MyClass < My_Orm::Class_Methods
- 
-end
-MyClass.set_table_name "Users2"
 
-MyClass.create_table({name:"varchar(30)",id:"INTEGER",age:"INTEGER"})
-puts MyClass.get_attr({name:"varchar",id:"int",age:"int"})
+class Users < My_Orm::Class_Methods
+
+end
+
+Users.read_the_table
+a = Users.new
+puts Users.instances.inspect
+
--- lib/my_orm/class_methods.rb
+++ lib/my_orm/class_methods.rb
@@ -1,41 +1,52 @@
 module My_Orm
-
     class Class_Methods
         attr_accessor :table_name
 
-        def self.set_table_name(table_name)
-            @table_name = table_name
-        end
+        
 
-        def self.create_table(attr)
-            structure = get_attr(attr)
-            puts "create table #{@table_name} (#{structure} );"
-            DataBase.request("create table #{@table_name} (
-                #{structure} );"
-            )
-            meta_column_to_properties(attr,self)
-        end
-        #attr - HASH {COLUMN: TYPE}
-        def self.get_attr(attr)
-            str = ''
-            commas = Array.new(attr.length,', ')
-            commas[-1] = ' '
-            i = 0
-            attr.each do |key,value|
-                str+= "#{key} #{value}" + commas[i]
-                i+=1
+         def self.read_the_table
+            info_about_columns = DataBase.request("PRAGMA table_info(#{self.to_s})")
+            info_about_rows = DataBase.request("select * from #{self}")
+            meta_create(self,info_about_columns,info_about_rows)
+         end
+
+
+
+         def self.add_field(table_name,field_name)
+            class_eval do
+                attr_accessor field_name.to_sym
             end
-            str
-        end
+         end
 
-
-        def meta_column_to_properties(attr,class_name)
-            attr.each do |key,_|
-                class_name.class_eval do
-                    attr_accessor "#{key}"
-                end
+         def self.meta_create(table_name,info_about_columns,info_about_rows)
+            columns = []
+            info_about_columns.each do |column|
+                columns << column[1]
+                add_field(table_name,column[1])
             end
-        end
+
+            create_the_instances(columns,info_about_rows)
+         end
+
+
+         
+
+         def self.create_the_instances(field_names,info_about_instance)
+            info_about_instance.each do |row|
+                create_the_instance(row,field_names)
+            end
+
+         end
+
+         def self.create_the_instance(row,field_names)
+            instance = new
+            
+            row.length.times do |i|
+                instance.instance_variable_set("@#{field_names[i]}",row[i])
+            end
+            
+         end
+         
     end
 
 end
